59ad03fd2cb0853181391d22e5df3cb2ada8323b
std-s <>
2022-12-09 15:06:37 +0800

    [Update] 初始化iOS。

--- ios/Classes/FlutterOpenimUnimpPlugin.m
+++ ios/Classes/FlutterOpenimUnimpPlugin.m
@@ -1,15 +1,75 @@
 #import "FlutterOpenimUnimpPlugin.h"
-#if __has_include(<flutter_openim_unimp/flutter_openim_unimp-Swift.h>)
-#import <flutter_openim_unimp/flutter_openim_unimp-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_openim_unimp-Swift.h"
-#endif
+#import "UniMPSDK/Core/Headers/DCUniMP.h"
 
 @implementation FlutterOpenimUnimpPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterOpenimUnimpPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"flutter_openim_unimp"
+                                     binaryMessenger:[registrar messenger]];
+    FlutterOpenimUnimpPlugin* instance = [[FlutterOpenimUnimpPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+    [registrar addApplicationDelegate:instance];
 }
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSMutableDictionary *option = launchOptions.mutableCopy;
+    option[@"debug"] = @YES;
+    [DCUniMPSDKEngine initSDKEnvironmentWithLaunchOptions:option];
+    return YES;
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([call.method isEqualToString:@"releaseWgtToRunPath"]) {
+        NSString *appID = call.arguments[@"appID"];
+        NSString *wgtPath = call.arguments[@"wgtPath" ];
+        NSString *password = call.arguments[@"password"];
+        NSError *err = nil;
+        
+        BOOL r = [DCUniMPSDKEngine installUniMPResourceWithAppid:appID
+                                                     resourceFilePath:wgtPath
+                                                             password:password
+                                                                error:&err];
+        result(r ? @(r) : err.userInfo);
+    } else if ([call.method isEqualToString:@"openUniMP"]) {
+        NSString *appID = call.arguments[@"appID"];
+        DCUniMPConfiguration *configuration = [DCUniMPConfiguration new];
+        configuration.enableBackground = YES;
+        [DCUniMPSDKEngine openUniMP:appID configuration:configuration completed:^(DCUniMPInstance * _Nullable uniMPInstance, NSError * _Nullable error) {
+            if (uniMPInstance != nil) {
+                NSLog(@"小程序打开成功");
+                result(@YES);
+            } else {
+                NSLog(@"打开小程序出错：%@", error);
+                result(error.userInfo);
+            }
+        }];
+    } else if ([call.method isEqualToString:@"closeUniMP"]) {
+        [DCUniMPSDKEngine closeUniMP];
+    }
+    else if ([call.method isEqualToString:@"getAppVersionInfo"]) {
+        NSString *appID = call.arguments[@"appID"];
+        NSDictionary *info = [DCUniMPSDKEngine getUniMPVersionInfoWithAppid:appID];
+        
+        result(info);
+    } else if ([call.method isEqualToString:@"sendUniMPEvent"]) {
+        NSString *appID = call.arguments[@"appID"];
+        NSString *event = call.arguments[@"event"];
+        NSString *data = call.arguments[@"data"];
+        
+        DCUniMPInstance *instance = [DCUniMPInstance new];
+        [instance sendUniMPEvent:event data:data];
+        
+        result(@YES);
+    } else if ([call.method isEqualToString:@"isInitialize"]) {
+        //                DCUniMPSDKEngine.is
+        //                result.success(DCUniMPSDK.getInstance().isInitialize());
+    } else if ([call.method isEqualToString:@"destory"]) {
+        [DCUniMPSDKEngine destory];
+    } else if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
 @end
--- ios/flutter_openim_unimp.podspec
+++ ios/flutter_openim_unimp.podspec
@@ -5,19 +5,28 @@
 Pod::Spec.new do |s|
   s.name             = 'flutter_openim_unimp'
   s.version          = '0.0.1'
-  s.summary          = 'A new Flutter project.'
+  s.summary          = 'A new Flutter plugin project.'
   s.description      = <<-DESC
-A new Flutter project.
+A new Flutter plugin project.
                        DESC
   s.homepage         = 'http://example.com'
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Your Company' => 'email@example.com' }
   s.source           = { :path => '.' }
-  s.source_files = 'Classes/**/*'
+  s.source_files = 'Classes/UniMPSDK/Core/Headers/*'
+  s.public_header_files = 'Classes/UniMPSDK/Core/Headers/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '9.0'
+  s.platform = :ios, '13.0'
 
+  s.resource_bundles = {
+    'Resources' => ['Classes/UniMPSDK/Core/Resources/*'],
+  }
+
+  s.vendored_libraries = 'Classes/**/*.a'
+  s.vendored_frameworks = 'Classes/**/*.framework'
+  s.frameworks = 'JavaScriptCore', 'CoreMedia', 'MediaPlayer', 'AVFoundation', 'AVKit', 'GLKit', 'OpenGLES', 'CoreText', 'QuartzCore', 'CoreGraphics', 'QuickLook', 'CoreTelephony', 'AssetsLibrary', 'CoreLocation', 'AddressBook'
+  
+  s.static_framework = true
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
-  s.swift_version = '5.0'
 end
